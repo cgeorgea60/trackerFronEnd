0928cbe85762569f892d913029aab027f8a6485a
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-02-25 10:41:15 +0100

    Modifications to Front-End

--- src/apis/taskFinder.js
+++ src/apis/taskFinder.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 require("dotenv").config();
+const url = process.env.baseURL;
 export default axios.create({
-  baseURL: process.env.baseURL,
+  baseURL: url,
   //baseURL: "http://localhost:5000/api/tasks",
 });
