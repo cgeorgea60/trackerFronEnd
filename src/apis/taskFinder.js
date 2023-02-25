863725b84eb2c6ff2f67a52ba7a567b4817f936f
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-02-25 10:45:36 +0100

    Removed .env

--- src/apis/taskFinder.js
+++ src/apis/taskFinder.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-require("dotenv").config();
-const url = process.env.baseURL;
+//require("dotenv").config();
+//const url = process.env.baseURL;
 export default axios.create({
-  baseURL: url,
+  baseURL: "https://tasktracker-api.up.railway.app/api/tasks",
   //baseURL: "http://localhost:5000/api/tasks",
 });
